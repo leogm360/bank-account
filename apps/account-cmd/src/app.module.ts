3c8716248a06e14740ef44899c97086f9c53a36e
leogm360 <80992604+leogm360@users.noreply.github.com>
2025-03-22 14:40:56 -0300

    fix: account-cmd dev

--- apps/account-cmd/src/app.module.ts
+++ apps/account-cmd/src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { HealthzModule } from 'api/healthz/loaders/healthz.module';
+import appConfigs from 'common/configs/app';
 
 @Module({
-  imports: [HealthzModule],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      load: [appConfigs],
+    }),
+    HealthzModule,
+  ],
 })
 export class AppModule {}
--- apps/account-cmd/src/main.ts
+++ apps/account-cmd/src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger, type INestApplication } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { type Express } from 'express';
 import { AppModule } from './app.module';
+import { type AppConfigs } from 'common/types/configs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<INestApplication<Express>>(AppModule);
 
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger('Bootstrap');
+  const configService = app.get<ConfigService>(ConfigService<AppConfigs>);
+
+  await app.listen(configService.get('APP').PORT);
+
+  if (configService.get('APP').NODE_ENV !== 'production') {
+    logger.log(`Server is running on ${await app.getUrl()}`);
+  }
 }
 
 bootstrap();
